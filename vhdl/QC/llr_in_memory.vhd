8aad0739abf8611e49f9b4fae6637813386d556d
calceushd <calceushd@gmail.com>
2018-08-13 19:34:52 +0200

    add in and output memory

--- vhdl/QC/llr_in_memory.vhd
+++ vhdl/QC/llr_in_memory.vhd
@@ -23,12 +23,38 @@ end entity;
 architecture base of llr_in_memory is
 	constant addr_bits : natural := integer(ceil(log2(real(HQC_COLUMNS))));
 	signal write_addr : unsigned(addr_bits-1 downto 0);
+    signal write_data, read_data : std_logic_vector(llr_column_t'length * llr_in(0)'length - 1 downto 0);
 begin
-	
+
 
 	process (clk)
 	begin
 		if rising_edge(clk) then
 			if res = '1' then
+                write_addr <= (others => '0');
+            else
+                if wr_in = '1' then
+                    write_addr <= write_addr + 1;
+                end if;
+            end if;
+        end if;
+    end process;
 
+    io_map : for i in llr_in'range generate
+        constant start : natural := (i + 1) * llr_in(0)'length - 1;
+        constant stop : natural := i * llr_in(0)'length;
+        write_data(start downto stop) <= std_logic_vector(llr_in(i));
+        llr_out(i) <= resize( signed(read_data(start downto stop), llr_out(0)'length);
+    end generate;
+
+    mem : entity work.generic_ram
+    port map (
+        clk => clk,
+        wr_en => wr_in,
+        wr_data => write_data,
+        wr_addr => write_addr,
+        rd_en => rd_in,
+        rd_data => read_data,
+        rd_addr => rd_in
+    );
 end architecture;
--- vhdl/QC/result_memory.vhd
+++ vhdl/QC/result_memory.vhd
@@ -4,4 +4,55 @@ use IEEE.numeric_std.all;
 use IEEE.math_real.all;
 use work.common.all;
 
+entity result_memory is
+    port (
+        clk : in std_logic;
+        res : in std_logic;
+        
+        res_in : in min_signs_t;
+        addr_in : in unsigned;
+        wr_in : in std_logic;
+
+        res_out : out min_signs_t;
+        res_rd : in std_logic;
+        res_end : out std_logic
+    );
+end entity;
+
+architecture base of result_memory is
+    constant addr_bits : natural := integer(ceil(log2(real(HQC_COLUMNS))));
+    signal read_addr : unsigned(addr_bits-1 downto 0);
+    signal write_data, read_data : std_logic_vector(min_signs_t'range);
+begin
+
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if res = '1' then
+                read_addr <= (others => '0');
+            else
+                if rd_in = '1' then
+                    read_addr <= read_addr + 1;
+                    if read_addr = HQC_COLUMNS then
+                        read_addr <= (others => '0');
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    write_data <= res_in;
+    res_out <= read_data;
+
+    mem : entity work.generic_ram
+    port map (
+        clk => clk,
+        wr_en => wr_in,
+        wr_data => write_data,
+        wr_addr => addr_in,
+        rd_en => res_rd,
+        rd_data => read_data,
+        rd_addr => read_addr
+    );
+end architecture;
 
