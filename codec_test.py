add correct calculation for Eb/N0 and parse it from command line, more iterations in qc python code

codec_test.py
@@ -3,9 +3,10 @@ import numpy
 import scipy.io as sio
 #import matplotlib.pyplot as plt
 from scipy.linalg import lu, circulant
-
+import sys
 import encode
 import decode
+import math
 
 
 Hqc = numpy.array([
@@ -21,9 +22,9 @@ Hqc = numpy.array([
     [11, -1, -1, -1, 19, -1, -1, -1, 13, -1,  3, 17, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  0, -1],
     [25, -1,  8, -1, 23, 18, -1, 14,  9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  0],
     [ 3, -1, -1, -1, 16, -1, -1,  2, 25,  5, -1, -1,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0]])
-Hqc = numpy.load("test_mat.npy")
-print(numpy.sum(Hqc >= 0, axis=1))
-print("aaaaaaaaaaaaaaaaaaa")
+#Hqc = numpy.load("test_mat.npy")
+#print(numpy.sum(Hqc >= 0, axis=1))
+#print("aaaaaaaaaaaaaaaaaaa")
 
 H = numpy.array([
         [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
@@ -37,7 +38,7 @@ H = numpy.array([
 #H = sio.loadmat("20003000V2.mat")
 #H = H["Hqc2"]
 
-expansion = 127
+expansion = 27
 
 block_vector = numpy.zeros(expansion, dtype=Hqc.dtype)
 block_vector[0] = 1
@@ -52,10 +53,15 @@ message_length = H.shape[1] - H.shape[0]
 ast = encode.encode_precompute(H, expansion)
 
 #block_vector[3] = 1
-        
-SIGMA = .1
+
+ebn0 = math.pow(10, float(sys.argv[1]) / 10)
+#print(ebn0)
+
+SIGMA = math.sqrt(.25 / ebn0)
 err_count = 0
-frame_count = 1
+bit_err_count = 0
+bit_err_count2 = 0
+frame_count = 1000
 
 #print(H)
 f = open("test.txt", "w")
@@ -66,23 +72,23 @@ def to_twoscomplement(value, bits):
     formatstring = '{:0%ib}' % bits
     return formatstring.format(value)
 
-for i in range(0, frame_count):
+for fc in range(0, frame_count):
     #if i % 10 == 0:
         #print(i)
     #encoding
     U = numpy.random.randint(2, size=(1,message_length))
     #M = numpy.transpose(encode.encode_message(U, G))
     M = encode.encode_ast(ast, U)
-    print(M.shape)
+    #print(M.shape)
     #channel
     e = numpy.random.standard_normal(M.shape) * SIGMA
-    X = M  #+ e
+    X = M + e
 
 
     #decoding
     
     LLR = (1 - 2 * X) / (2 * SIGMA)
-    
+    """
     test = numpy.reshape(LLR, (-1, block_vector.shape[0])) * 10
     test = test.astype(int)
     
@@ -90,19 +96,23 @@ for i in range(0, frame_count):
         for j in range(0, test.shape[1]):
             f.write(to_twoscomplement(test[i,j],8))
         f.write("\n")
-    
+    """
     #LLR = test
     #print(LLR.shape)
-    #Xe = decode.decode_soft(LLR, H)[:,0]
+    Xe2 = decode.decode_soft(LLR, H)[:,0]
     Xe = decode.decode_qc(LLR, Hqc, block_vector)
-    if (numpy.sum(Xe != M[:,0]) != 0):
-        print("wrong")
+    su = numpy.sum(Xe != M[:,0])
+    bit_err_count += su
+    bit_err_count2 += numpy.sum(Xe2 != M[:,0])
+    if (su != 0):
+        #print("wrong")
         err_count += 1
-    else:
-        print("right")
-    if i % 1 == 0:
-        print("frame error rate:", err_count / (i+1.0))
-print("frame error rate:", err_count / frame_count)
+    #else:
+        #print("right")
+    if fc % 1 == 0:
+        print("frame error rate:", float(err_count) / (fc + 1), " BER:", bit_err_count / (fc + 1) / M.shape[0], bit_err_count2 / (fc + 1) / M.shape[0])
+
+print(sys.argv[1], err_count / frame_count, bit_err_count / frame_count / M.shape[0])
 
 f.close()
 