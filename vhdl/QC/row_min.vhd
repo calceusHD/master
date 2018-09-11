3a8307bcce2b282d691cb0a0efbb960a4503dbae
calceushd <calceushd@gmail.com>
2018-09-11 13:22:34 +0200

    fix row min calculation so that min2 is calculated

--- vhdl/QC/row_min.vhd
+++ vhdl/QC/row_min.vhd
@@ -30,6 +30,9 @@ begin
 			row_tmp := to_unsigned(to_integer(abs(row_in(i))), row_tmp'length);
 			--report "Row_tmp" & to_hstring(row_tmp);
 			--report "Min_rv" & to_hstring(min_rv);
+			if row_tmp > min_rv and row_tmp < min2_rv then
+				min2_rv := row_tmp;
+			end if;
 			if row_tmp <= min_rv then
 				min2_rv := min_rv;
 				min_rv := row_tmp;
