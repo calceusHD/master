c3622aed28fbd59b4b43c3f5f50549de02d419a1
calceushd <calceushd@gmail.com>
2018-09-21 13:31:28 +0200

    add last option to bit_repack, fix bit_repack

--- vhdl/QC/bit_repack.vhd
+++ vhdl/QC/bit_repack.vhd
@@ -5,6 +5,9 @@ use IEEE.math_real.all;
 
 
 entity bit_repack is
+	generic (
+		LAST_WORD_PADDING : natural := 0
+	);
 	port (
 		clk : in std_logic;
 		res : in std_logic;
@@ -12,41 +15,60 @@ entity bit_repack is
 		out_bits : out std_logic_vector;
 		in_rdy : out std_logic;
 		in_wr : in std_logic;
+		in_last : in std_logic;
 		out_rdy : in std_logic;
-		out_wr : out std_logic
+		out_wr : out std_logic;
+		out_last : out std_logic
 	);
 end entity;
 
 architecture base of bit_repack is
 	signal bit_store : std_logic_vector(out_bits'length + in_bits'length - 1 downto 0);
 	signal out_wr_int : std_logic;
-	signal store_cnt : unsigned(7 downto 0) := (others => '0');
+	signal store_cnt : unsigned(12 downto 0) := (others => '0');
 	signal in_rdy_int : std_logic := '0';
 	signal zero_pad : std_logic_vector(out_bits'range) := (others => '0');
+	signal flusing : std_logic := '0';
 begin
 	out_wr <= out_wr_int;
 	in_rdy <= in_rdy_int;
 
-	in_rdy_int <= '1' when store_cnt <= bit_store'length - in_bits'length  and res = '0' else '0';
-	out_wr_int <= '1' when store_cnt >= out_bits'length and res = '0' and out_rdy = '1' else '0';
-
+	in_rdy_int <= '1' when in_bits'length <= bit_store'length + out_bits'length - store_cnt  and res = '0' and flusing = '0' else '0';
+	
+	
 	process (clk)
 		variable store_cnt_tmp : unsigned(store_cnt'range);
 		variable bit_store_tmp : std_logic_vector(bit_store'range);
 	begin
 		if rising_edge(clk) then
+			if in_last = '1' then
+				flusing <= '1';
+			end if;
+			if out_last = '1' then
+				out_last <= '0';
+			end if;
 			bit_store_tmp := bit_store;
 			store_cnt_tmp := store_cnt;
-			if out_wr_int = '1' then
-				bit_store_tmp := std_logic_vector(shift_right(unsigned(bit_store_tmp), out_bits'length));
-				out_bits <= bit_store(out_bits'range);
+			if store_cnt >= out_bits'length and res = '0' and out_rdy = '1' then
+				out_wr_int <= '1';
+				bit_store_tmp := std_logic_vector(shift_left(unsigned(bit_store_tmp), out_bits'length));
+				out_bits <= bit_store(bit_store'length-1 downto bit_store'length - out_bits'length);
 				store_cnt_tmp := store_cnt_tmp - out_bits'length;
+			else
+				out_wr_int <= '0';
 			end if;
 			if in_wr = '1' then
-				bit_store_tmp := bit_store_tmp or std_logic_vector(shift_left(unsigned(in_bits & zero_pad), to_integer(store_cnt_tmp)));
+				bit_store_tmp := bit_store_tmp or std_logic_vector(shift_right(unsigned(in_bits & zero_pad), to_integer(store_cnt_tmp)));
 				store_cnt_tmp := store_cnt_tmp + in_bits'length;
 			end if;
+			if flusing = '1' and store_cnt_tmp = LAST_WORD_PADDING then
+				store_cnt_tmp := (others => '0');
+				flusing <= '0';
+				out_last <= '1';
+			end if;
 			if res = '1' then
+				flusing <= '0';
+				out_last <= '0';
 				bit_store <= (others => '0');
 			else
 				bit_store <= bit_store_tmp;
--- vhdl/QC/bit_repack_tb.vhd
+++ vhdl/QC/bit_repack_tb.vhd
@@ -11,10 +11,10 @@ entity bit_repack_tb is
 end entity;
 
 architecture test of bit_repack_tb is
-	signal clk, res, in_wr, out_rdy : std_logic := '0';
+	signal clk, res, in_wr, out_rdy, in_last : std_logic := '0';
 	signal in_rdy, out_wr : std_logic;
 	signal in_bits : std_logic_vector(31 downto 0) := (others => '0');
-	signal out_bits : std_logic_vector(4 downto 0);
+	signal out_bits : std_logic_vector(20 downto 0);
 
     procedure read_padded_vec(variable line_in : inout line; rv : out std_logic_vector; good : out boolean) is
         variable good_int : boolean := True;
@@ -45,23 +45,35 @@ begin
 		res <= '1';
 		wait for 10 ns;
 		res <= '0';
-        wait for 10 ns;
+        wait for 15 ns;
 
 		file_open(test_data, "../../test.txt", read_mode);
         
         while not endfile(test_data) loop
             report "start loop";
             readline(test_data, in_line);
+			good := true;
             while good loop
                 report "start inner loop";
                 read_padded_vec(in_line, vec_temp, good);
-                wait until rising_edge(clk);
+				wait for 10 ns;
+				in_last <= '0';
+				while in_rdy /= '1' loop
+					in_wr <= '0';
+					wait for 10 ns;
+					report "looperooni";
+				end loop;
                 report "got clock";
-                in_wr <= '0';
-                wait on ckl until in_rdy = '1';
+                --in_wr <= '0';
+                --wait on ckl until in_rdy = '1';
                 report "got ready";
                 in_wr <= '1';
                 in_bits <= vec_temp;
+				if not good then
+					in_last <= '1';
+				else
+					in_last <= '0';
+				end if;
             end loop;
 		end loop;
 		in_wr <= '0';
@@ -70,12 +82,15 @@ begin
 
 	process
 	begin
-		wait for 60 ns;
+		wait for 20 ns;
 		out_rdy <= '1';
 		wait;
 	end process;
 
 	dut : entity work.bit_repack
+	generic map (
+		LAST_WORD_PADDING => 3
+	)
 	port map (
 		clk => clk,
 		res => res,
@@ -83,6 +98,7 @@ begin
 		out_bits => out_bits,
 		in_rdy => in_rdy,
 		in_wr => in_wr,
+		in_last => in_last,
 		out_rdy => out_rdy,
 		out_wr => out_wr
 	);
