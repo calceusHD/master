77bc9a1b2e8ee6b801a8d3beb8e1d09212c90c22
calceushd <calceushd@gmail.com>
2018-10-01 13:53:57 +0200

    mux based barrel shifter and testbench

--- vhdl/QC/dynamic_roll.vhd
+++ vhdl/QC/dynamic_roll.vhd
@@ -44,3 +44,23 @@ begin
 	end generate;
 
 end architecture;
+
+architecture log2 of dynamic_roll is
+	type temp_type is array(0 to roll_count'length) of llr_array_t;
+	signal temp : temp_type;
+begin
+	temp(0) <= data_in;
+	gen_bits : for bit_id in 0 to temp'length-2 generate
+		signal cnt : integer;
+	begin
+		cnt <= 2 ** bit_id when DIRECTION else - 2 ** bit_id;
+		gen_i : for i in data_in'range(1) generate
+			gen_j : for j in data_in'range(2) generate
+					temp(bit_id + 1)(i, j) <= temp(bit_id)((i + cnt) mod data_in'length, j) 
+						when roll_count(bit_id) = '1' 
+						else temp(bit_id)(i, j);
+			end generate;
+		end generate;
+	end generate;
+	data_out <= temp(temp'length-1);
+end architecture;
---
+++ vhdl/QC/dynamic_roll_tb.vhd
@@ -0,0 +1,52 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.math_real.all;
+use work.common.all;
+
+entity dynamic_roll_tb is
+end entity;
+
+architecture base of dynamic_roll_tb is
+	signal data_in, data_out, data_ref : llr_array_t;
+	signal roll_count : roll_t;
+begin
+
+	process
+	begin
+		for i in data_in'range(1) loop
+			data_in(i, 0) <= to_signed(i, data_in(0, 0)'length);
+		end loop;
+		roll_count <= (others => '0');
+		for i in 0 to 199 loop
+		wait for 10 ns;
+			roll_count <= roll_count + 1;
+			wait for 10 ns;
+			assert data_out = data_ref;
+		end loop;
+		wait;
+	end process;
+
+	dut : entity work.dynamic_roll(log2)
+	generic map (
+		DIRECTION => false
+	)
+	port map (
+		roll_count => roll_count,
+		data_in => data_in,
+		data_out => data_out
+	);
+
+	ref : entity work.dynamic_roll(base)
+	generic map (
+		DIRECTION => false
+	)
+	port map (
+		roll_count => roll_count,
+		data_in => data_in,
+		data_out => data_ref
+	);
+		
+end architecture;
+
+
