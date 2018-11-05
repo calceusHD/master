634af3ade7a7f02e1d91baa21faf4e09887f6354
calceushd <calceushd@gmail.com>
2018-11-05 15:16:06 +0100

    add results comparison block

--- vhdl/QC/axi_cmp.vhd
+++ vhdl/QC/axi_cmp.vhd
@@ -24,17 +24,47 @@ entity axi_cmp is
 end entity;
 
 architecture base of axi_cmp is
-	signal valid : std_logic;
+	signal valid, ready : std_logic;
+	signal m_tvalid_int : std_logic;
+	signal counter : unsigned(31 downto 0) := (others => '0');
+	
+	function count_ones(v : std_logic_vector) return unsigned is
+        variable tmp : unsigned(31 downto 0);
+    begin
+        tmp := (others => '0');
+        for i in v'range loop
+            if v(i) = '1' then
+                tmp := tmp + 1;
+            end if;
+        end loop;
+        return tmp;
+    end function;
 begin
+    m_tdata <= std_logic_vector(counter);
+    m_tvalid <= m_tvalid_int;
 	valid <= s_tvalid and s2_tvalid;
-	s_tready <= valid;
-	s2_tready <= valid;
+	s_tready <= valid and ready;
+	s2_tready <= valid and ready;
+	m_tlast <= '1';
 	process (clk)
 	begin
 		if rising_edge(clk) then
-			if valid = '1' then
-				assert s_tdata = s2_tdata report "oh no" severity warning;
+            if res_e = '1' then
+                m_tvalid_int <= '0';
+                counter <= (others => '0');
+                ready <= '1';
+			elsif valid = '1' and ready = '1' then
+                counter <= counter + count_ones(s_tdata xor s2_tdata);
+                if s_tlast = '1' then
+                    ready <= '0';
+                    m_tvalid_int <= '1';
+                end if;
 			end if;
+			if m_tvalid_int = '1' and m_tready = '1' then
+                m_tvalid_int <= '0';
+                ready <= '1';
+                counter <= (others => '0');
+            end if;
 		end if;
 	end process;
 end architecture;
